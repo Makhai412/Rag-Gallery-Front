551da4973a6ac6ae7954ea99633b19d54dd50a3f
Makhai412 <higueralozanoandres@gmail.com>
2024-10-17 19:17:38 -0500

    chat con icono add file

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,6 @@
 import { useState, useRef } from "react";
 import axios from "axios";
 
-
 const Chat = () => {
     const [messages, setMessages] = useState([
         { text: "¿Cómo te puedo ayudar hoy?", time: new Date().toLocaleTimeString(), sender: 'system' }
@@ -13,18 +12,18 @@ const Chat = () => {
 
     const handleSendMessage = async (e) => {
         e.preventDefault();
-        if (isSending) return;
+        if (isSending || inputValue.trim() === "") return;
 
         const timestamp = new Date().toLocaleTimeString();
+        await handleTextMessage(timestamp);
+    };
 
-        // Si hay un archivo seleccionado, sube el archivo.
-        if (file) {
-            await handleFileUpload(timestamp);
-        } 
-        // Si hay un mensaje, envíalo.
-        else if (inputValue.trim() !== "") {
-            await handleTextMessage(timestamp);
-        }
+    const handleSendFile = async (e) => {
+        e.preventDefault();
+        if (isSending || !file) return;
+
+        const timestamp = new Date().toLocaleTimeString();
+        await handleFileUpload(timestamp);
     };
 
     const handleTextMessage = async (timestamp) => {
@@ -57,13 +56,12 @@ const Chat = () => {
     };
 
     const handleFileUpload = async (timestamp) => {
-        if (!file) return;
         setIsSending(true);
         const formData = new FormData();
         formData.append('file', file);
 
         try {
-            const response = await axios.post("http://127.0.0.1:8001/upload", formData, {
+            await axios.post("http://127.0.0.1:8001/upload", formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
@@ -93,10 +91,13 @@ const Chat = () => {
     };
 
     return (
-        <div className="flex flex-col  h-screen p-4 mt-2 py-8 bg-blue-300">
-            <div className="flex-1 overflow-y-auto  w-3/4 mx-auto p-4 border bg-white border-gray-300 rounded-lg mt-16 bg-cover bg-no-repeat bg-center" 
-                style={{backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('https://i.ibb.co/tD1648m/Rag-Gallery-Logo.png')`,
-                        backgroundSize: 'contain' }}>
+        <div className="flex flex-col h-screen p-4 mt-2 py-8 bg-blue-300">
+            <div className="flex-1 overflow-y-auto w-3/4 mx-auto p-4 border bg-white border-gray-300 rounded-lg mt-16 bg-cover bg-no-repeat bg-center" 
+                style={{
+                    backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url('https://i.ibb.co/tD1648m/Rag-Gallery-Logo.png')`,
+                    backgroundSize: '300px', 
+                    backgroundPosition: 'center' 
+                }}>
                 {messages.map((message, index) => (
                     <div key={index} className={`flex items-start gap-2.5 mb-2 ${message.sender === 'user' ? 'justify-end' : ''}`}>
                         {message.sender === 'user' ? (
@@ -112,11 +113,7 @@ const Chat = () => {
                             </div>
                         ) : (
                             <>
-                                <img 
-                                    className="w-8 h-8 rounded-full" 
-                                    src="/docs/images/people/system-avatar.jpg" 
-                                    alt="System Avatar" 
-                                />
+                                <img className="w-8 h-8 rounded-full" src="/docs/images/people/system-avatar.jpg" alt="System Avatar"/>
                                 <div className="flex flex-col gap-1 w-full max-w-[320px]">
                                     <div className="flex items-center space-x-2 rtl:space-x-reverse">
                                         <span className="text-sm font-semibold text-gray-900 dark:text-white">Sistema</span>
@@ -132,29 +129,18 @@ const Chat = () => {
                     </div>
                 ))}
             </div>
-            <form onSubmit={handleSendMessage} className="mt-4  w-3/4 mx-auto flex">
-                <button type="button" 
-                    onClick={() => fileInputRef.current.click()} 
-                    className="p-2 rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
-                    </svg>
+            <form className="mt-4 w-3/4 mx-auto flex">
+                <input type="file" ref={fileInputRef} onChange={handleFileChange} style={{ display: 'none' }} />
+                <button type="button" onClick={() => fileInputRef.current.click()} className="p-2 rounded-full bg-blue-500 hover:bg-blue-700">
+                    <img src="https://i.ibb.co/PjRxhP2/clip-de-papel-de-metal.png" alt="Clip de papel" className="w-6 h-6"/>
                 </button>
-                <input 
-                    type="file" 
-                    ref={fileInputRef} 
-                    onChange={handleFileChange} 
-                    style={{ display: 'none' }} 
-                />
-                <input type="text" className="flex-1 p-2 border border-gray-300 rounded-lg" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
-                    placeholder="Escribe un mensaje..." disabled={isSending}
-                />
-                <button 
-                    type="submit" 
-                    className="ml-2 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700" 
-                    disabled={isSending}
-                >
-                    {isSending ? "Enviando..." : "Enviar"}
+                <button onClick={handleSendFile} className="ml-2 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700" disabled={isSending || !file}>
+                    {isSending ? "Enviando..." : "Enviar Archivo"}
+                </button>
+                <input type="text" className="flex-1 p-2 border border-gray-300 rounded-lg ml-2" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Escribe un mensaje..." disabled={isSending}/>
+                <button onClick={handleSendMessage} className="ml-2 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700" disabled={isSending}>
+                    {isSending ? "Enviando..." : "Enviar Mensaje"}
                 </button>
             </form>
         </div>
