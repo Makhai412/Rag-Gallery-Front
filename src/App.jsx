2fbbe4bb97b0a19c81253f773a14f018653c7d0a
Andres Higuera <higueralozanoandres@gmail.com>
2024-10-23 00:17:50 -0500

    Chat Funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import NavbarComponent from "./components/navbar/NavBarComponent";
 import AppRoutes from "./routes/AppRoutes";
-import UploadInterface from "./components/navbar/UploadInterface";
 
 function App() {
   return (
     <main>
       <NavbarComponent />
       <AppRoutes />
-      <UploadInterface/>
+
     </main>
   );
 }
